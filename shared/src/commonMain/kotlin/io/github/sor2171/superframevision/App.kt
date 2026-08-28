b7bd3de37fbe10153db01b67b77d9b5e2a3acb20
SOR <3272194354@qq.com>
2026-08-28 16:21:59 +0800

    feat: better base UI

--- shared/src/commonMain/kotlin/io/github/sor2171/superframevision/App.kt
+++ shared/src/commonMain/kotlin/io/github/sor2171/superframevision/App.kt
@@ -5,19 +5,18 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationRail
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -28,12 +27,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import io.github.sor2171.superframevision.core.entity.Screens
 import io.github.sor2171.superframevision.core.entity.currentPlatform
+import io.github.sor2171.superframevision.core.entity.title
 import io.github.sor2171.superframevision.core.utils.SettingsRepository
-import io.github.sor2171.superframevision.ui.component.ScreenButtonBar
-import io.github.sor2171.superframevision.ui.component.ScreenButtonRail
 import io.github.sor2171.superframevision.ui.screens.HomeScreen
 
 @Composable
@@ -47,78 +44,53 @@ fun App() {
         SettingsRepository.load()
     }
 
-    @Composable
-    fun NavigationRailItems() {
-        Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Screens.entries.forEach { screen ->
-                ScreenButtonRail(
-                    currentScreen = currentScreen,
-                    screen = screen,
-                    onClick = { currentScreen = screen }
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun RowScope.NavigationBarItems() {
-        Screens.entries.forEach { screen ->
-            ScreenButtonBar(
-                currentScreen = currentScreen,
-                screen = screen,
-                onClick = { currentScreen = screen }
-            )
-        }
-    }
-
-    val homeScreen = @Composable {
-        HomeScreen(
-            platform = platform
-        )
-    }
-
     MaterialTheme {
-        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-            val showNavigationRail = maxWidth > 600.dp
-
-            Row(modifier = Modifier.fillMaxSize()) {
-                if (showNavigationRail) {
-                    NavigationRail(
-                        containerColor = NavigationBarDefaults.containerColor
-                    ) {
-                        NavigationRailItems()
+        Row(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            NavigationRail(
+                containerColor = NavigationBarDefaults.containerColor
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Screens.entries.forEach { screen ->
+                        NavigationRailItem(
+                            selected = currentScreen == screen,
+                            onClick = { currentScreen = screen },
+                            icon = { Icon(screen.icon, contentDescription = screen.title()) },
+                            label = { Text(screen.title()) }
+                        )
                     }
                 }
+            }
 
-                Scaffold(
-                    modifier = Modifier.weight(1f),
-                    bottomBar = {
-                        if (!showNavigationRail) {
-                            NavigationBar {
-                                NavigationBarItems()
-                            }
-                        }
-                    }
-                ) { innerPadding ->
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        AnimatedContent(
-                            targetState = currentScreen,
-                            transitionSpec = { fadeIn().togetherWith(fadeOut()) },
-                            label = "ScreenTransition"
-                        ) { screen ->
-                            when (screen) {
-                                Screens.Home -> homeScreen()
-                                else -> {}
-                            }
-                        }
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                AnimatedContent(
+                    targetState = currentScreen,
+                    transitionSpec = { fadeIn().togetherWith(fadeOut()) },
+                    label = "ScreenTransition"
+                ) { screen ->
+                    when (screen) {
+                        Screens.Home -> HomeScreen(
+                            platform = platform
+                        )
+
+                        Screens.Process -> HomeScreen(
+                            platform = platform
+                        )
+
+                        Screens.Settings -> HomeScreen(
+                            platform = platform
+                        )
+
+                        Screens.Info -> HomeScreen(
+                            platform = platform
+                        )
                     }
                 }
             }
