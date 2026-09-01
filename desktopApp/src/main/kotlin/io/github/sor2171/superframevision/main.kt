58570ddc0b072533d81e5accc96c39b94e1103c7
SOR <3272194354@qq.com>
2026-09-01 11:43:42 +0800

    feat: icon for xml

--- desktopApp/src/main/kotlin/io/github/sor2171/superframevision/main.kt
+++ desktopApp/src/main/kotlin/io/github/sor2171/superframevision/main.kt
@@ -7,14 +7,15 @@ import androidx.compose.ui.window.rememberWindowState
 import io.github.sor2171.superframevision.core.utils.Const
 import org.jetbrains.compose.resources.painterResource
 import superframevision.shared.generated.resources.Res
+import superframevision.shared.generated.resources.kmp
 import superframevision.shared.generated.resources.sfv
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = Const.FULL_APP_NAME,
-        state = rememberWindowState(width = 1100.dp, height = 800.dp),
-        icon = painterResource(Res.drawable.sfv)
+        state = rememberWindowState(width = 900.dp, height = 800.dp),
+        icon = painterResource(Res.drawable.kmp)
     ) {
         App()
     }
