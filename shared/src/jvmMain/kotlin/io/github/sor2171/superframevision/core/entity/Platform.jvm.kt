0b68b36d11e748a52fd7255f9e87bbdfd03d45bc
SOR <3272194354@qq.com>
2026-09-10 11:14:18 +0800

    refactor: new method to judge platform

--- shared/src/jvmMain/kotlin/io/github/sor2171/superframevision/core/entity/Platform.jvm.kt
+++ shared/src/jvmMain/kotlin/io/github/sor2171/superframevision/core/entity/Platform.jvm.kt
@@ -1,21 +1,26 @@
 package io.github.sor2171.superframevision.core.entity
 
-actual fun currentPlatform(): Platform {
-        val os = System.getProperty("os.name").lowercase()
-        val arch = System.getProperty("os.arch").lowercase()
+import com.sun.jna.Platform.is64Bit
+import com.sun.jna.Platform.isARM
+import com.sun.jna.Platform.isIntel
+import com.sun.jna.Platform.isLinux
+import com.sun.jna.Platform.isMac
+import com.sun.jna.Platform.isWindows
 
-        return Platform(
-            os = when {
-                os.contains("windows") -> Platform.Os.Windows
-                os.contains("linux") -> Platform.Os.Linux
-                os.contains("mac") || os.contains("darwin") -> Platform.Os.MacOS
-                else -> error("Unsupported OS: $os")
-            },
-            architecture = when (arch) {
-                "x86" -> Platform.Architecture.X86
-                "amd64", "x86_64" -> Platform.Architecture.X86_64
-                "aarch64", "arm64" -> Platform.Architecture.Arm64
-                else -> error("Unsupported architecture: $arch")
-            }
-        )
-    }
+actual fun currentPlatform(): Platform {
+
+    return Platform(
+        os = when {
+            isWindows() -> Platform.Os.Windows
+            isLinux() -> Platform.Os.Linux
+            isMac() -> Platform.Os.MacOS
+            else -> error("Unsupported OS: ${System.getProperty("os.name")}")
+        },
+        architecture = when {
+            !is64Bit() -> Platform.Architecture.X86
+            isIntel() && is64Bit() -> Platform.Architecture.X86_64
+            isARM() && is64Bit() -> Platform.Architecture.Arm64
+            else -> error("Unsupported architecture: ${System.getProperty("os.arch")}")
+        }
+    )
+}
